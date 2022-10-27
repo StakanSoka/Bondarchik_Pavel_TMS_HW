d915c1fd9b85ea8306f835b6d348c49419e2627e
Bondarchik Pavel <tmsbonarchik02@gmail.com>
2022-10-27 21:40:21 +0300

    evergreen

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import static java.lang.Math.*;
+
 public class Main {
+    static void printEverGreen (int size)  {
+        for (int j = 0; j < size; j++) {
+            System.out.print(' ');
+        }
+        System.out.println('*');
+
+        for (int i = size - 1; i != 1; i--){
+            for (int j = 0; j < i; j++) {
+                System.out.print(' ');
+            }
+            System.out.print('*');
+
+            for (int j = 0; j < (size - i) * 2 - 1; j++){
+                System.out.print(' ');
+            }
+            System.out.println('*');
+        }
+
+        System.out.print(' ');
+        for (int i = 0; i < size * 2; i++) {
+            if (i % 2 == 0) {
+                System.out.print('*');
+            }
+            else System.out.print(' ');
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("\u0048\u0065\u006C\u006C\u006F \u0057\u006F\u0072\u006C\u0064\u0021");
+        int number = 30;
+        printEverGreen(number);
     }
 }
