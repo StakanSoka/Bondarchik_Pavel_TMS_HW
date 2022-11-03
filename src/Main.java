14b51969e7069d57ce34c81bde6662e96f6291c9
Bondarchik Pavel <tmsbonarchik02@gmail.com>
2022-11-03 19:51:02 +0300

    pullreq

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,6 @@
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
-    static void printEverGreen (int size)  {
-        for (int j = 0; j < size; j++) {
-            System.out.print(' ');
-        }
-        System.out.println('*');
-
-        for (int i = size - 1; i != 1; i--){
-            for (int j = 0; j < i; j++) {
-                System.out.print(' ');
-            }
-            System.out.print('*');
-
-            for (int j = 0; j < (size - i) * 2 - 1; j++){
-                System.out.print(' ');
-            }
-            System.out.println('*');
-        }
-
-        System.out.print(' ');
-        for (int i = 0; i < size * 2; i++) {
-            if (i % 2 == 0) {
-                System.out.print('*');
-            }
-            else System.out.print(' ');
-        }
-    } // I was bored
 
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
