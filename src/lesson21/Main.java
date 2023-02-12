46bbbaecaf4f05c575602e70db3499e93fd428cf
Bondarchik Pavel <tmsbonarchik02@gmail.com>
2023-02-12 16:41:00 +0300

    Code fixes v2

--- src/lesson21/Main.java
+++ src/lesson21/Main.java
@@ -1,35 +1,42 @@
 package lesson21;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Main {
 
     public static final String BASED_PATH = "src\\lesson21\\files\\data.txt";
     public static String separatedCharacter = ",";
 
-    public static void readPeopleFromTheFile(File file, ArrayList<Person> people) {
+    public static List<Person> readPeopleFromTheFile(File file) {
+        List<Person> people = new ArrayList<>();
         String line;
         String[] personData;
-        try (BufferedReader reader = new BufferedReader(new FileReader(file.getPath()))) {
-            while ((line = reader.readLine()) != null) {
-                personData = line.split(separatedCharacter);
-                people.add(new Person(personData[0], personData[1],
-                        personData[2], Integer.parseInt(personData[3].trim())));
-            }
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(file.getPath()))) {
+            Person person;
+            while ((line = reader.readLine()) != null) {
+                person = new Person();
+                personData = line.split(separatedCharacter);
+
+                person.setFirstName(personData[0].trim());
+                person.setLastName(personData[1].trim());
+                person.setSex(personData[2].trim());
+                person.setAge(Integer.parseInt(personData[3].trim()));
+
+                people.add(person);
+            }
         } catch (FileNotFoundException exception) {
             System.out.println("There is no file with path + " + file.getPath());
             System.exit(0);
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
         }
+
+        return people;
     }
 
-    public static void rewriteFile(File file, ArrayList<Person> people) {
+    public static void rewriteFile(File file, List<Person> people) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file.getPath()))) {
             for (Person person : people) {
                 writer.write(person.getFirstName() + separatedCharacter + person.getLastName() + separatedCharacter +
@@ -46,23 +53,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         File file;
-        ArrayList<Person> people = new ArrayList<>();
-        int ageCount30;
-        int maleCount;
-        int femaleCount;
+        List<Person> people;
+        int ageCount30, maleCount, femaleCount;
 
         System.out.println("Enter the path of the file");
         file = new File(scanner.nextLine());
 
-        readPeopleFromTheFile(file, people);
+        people = readPeopleFromTheFile(file);
 
         people.sort((p1, p2) -> (p1.getFirstName() + p1.getLastName()).compareTo(p2.getFirstName() + p2.getLastName()));
 
         rewriteFile(file, people);
 
         ageCount30 = (int) people.stream().filter(o -> o.getAge() > 30).count();
-        maleCount = (int) people.stream().filter(o -> o.getSex().trim().equals("male")).count();
-        femaleCount = (int) people.stream().filter(o -> o.getSex().trim().equals("female")).count();
+        maleCount = (int) people.stream().filter(o -> o.getSex().equals("male")).count();
+        femaleCount = (int) people.stream().filter(o -> o.getSex().equals("female")).count();
 
         System.out.println("Count : " + ageCount30);
         System.out.println("Male : " + maleCount);
--- src/lesson21/Person.java
+++ src/lesson21/Person.java
@@ -1,20 +1,11 @@
 package lesson21;
 
-import java.util.ArrayList;
-
 public class Person {
 
     private String firstName;
     private String lastName;
     private String sex;
     private int age;
-
-    public Person(String firstName, String lastName, String sex, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.sex = sex;
-        this.age = age;
-    }
 
     public String getFirstName() {
         return firstName;
@@ -30,6 +21,22 @@ public class Person {
 
     public String getSex() {
         return sex;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     @Override
